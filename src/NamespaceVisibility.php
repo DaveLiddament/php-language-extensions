5a2bbc10bf3720631bb7c4a3d98a41723c07cea0
Dave Liddament <dave@lampbristol.com>
2022-12-03 18:05:43 +0000

    ADD NamespaceVisibility attribute

---
+++ src/NamespaceVisibility.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaveLiddament\PhpLanguageExtensions;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
+final class NamespaceVisibility
+{
+    public function __construct(
+        ?string $namespace = null,
+        bool $excludeSubNamespaces = false,
+    ) {
+    }
+}
